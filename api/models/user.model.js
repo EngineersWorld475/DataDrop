def906f54acdd3fb3dc15337a735c97dc161c16f
EngineersWorld457 <sanjaygnair777@gmail.com>
2024-07-01 17:00:23 -0700

    update header component with user data

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = mongoose.Schema(
     profilePicture: {
       type: String,
       default:
-        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        'https://storage.needpix.com/rsynced_images/user-1808597_1280.png',
     },
   },
   { timestamps: true }
