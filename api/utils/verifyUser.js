a7fa8250727e788232ecee170dc62192f94d5bd4
EngineersWorld457 <sanjaygnair777@gmail.com>
2024-07-02 21:51:11 -0700

    update user API route

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+import { errorHandler } from './error.js';
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  console.log('...token', token);
+  if (!token) {
+    return next(errorHandler(401, 'Unauthorized'));
+  }
+  let user = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = user;
+  next();
+};
