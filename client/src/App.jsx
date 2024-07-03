047c4c32b7e6c647c33f859ac811b22a0df34a21
EngineersWorld457 <sanjaygnair777@gmail.com>
2024-07-02 23:58:40 -0700

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl text-red-500">App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">About Page</h1>
+    </div>
+  );
+};
+
+export default About;
