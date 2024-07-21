57205d04b40003875c222690e3afd6c0fb318950
EngineersWorld457 <sanjaygnair777@gmail.com>
2024-07-21 04:32:39 -0700

    add posts section to the dashboard

---
+++ client/src/components/DashPost.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DashPost = () => {
+  return <div>DashPosts</div>;
+};
+
+export default DashPost;
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,10 +1,15 @@
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight } from 'react-icons/hi';
-import React from 'react';
-import { useDispatch } from 'react-redux';
-
+import { HiUser, HiArrowSmRight, HiDocumentText } from 'react-icons/hi';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { signOutSuccess } from '../redux/user/userSlice';
 const DashSidebar = () => {
   const dispatch = useDispatch();
+  const [tab, setTab] = useState('');
+  const location = useLocation();
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const handleSignOut = async () => {
     try {
       const res = await fetch(`/api/user/signout`, {
@@ -15,17 +20,49 @@ const DashSidebar = () => {
         console.log(data.message);
       }
       dispatch(signOutSuccess());
+      navigate('/signin');
     } catch (error) {
       console.log(error.message);
     }
   };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    console.log(urlParams);
+    const tabFromUrl = urlParams.get('tab');
+    console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
     <Sidebar className="w-full">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item active icon={HiUser} label={'User'} labelColor="dark">
-            Profile
-          </Sidebar.Item>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                labelColor="dark"
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+
           <Sidebar.Item
             icon={HiArrowSmRight}
             className="cursor-pointer"
